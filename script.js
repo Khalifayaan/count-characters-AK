701cfdbc4e1c2f11b75b0c9352c67f6ac8773f60
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-17 11:31:34 +0300

    count-characters assignment done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Good Luck 💪🏾
+
+
+
+function count(character, string) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === character) {
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  
+  const result = count("m", "communication");
+  console.log(result);
